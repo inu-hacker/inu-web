4bd2839d6cfbd395b6979529f5bd8aec3432c82e
pinknoisebabies <pinknoisebabies@run-girls.run>
2023-06-25 03:10:49 +0900

    add pages workflows

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,8 @@ import UnoCSS from 'unocss/astro';
 
 export default defineConfig({
   // used to generate images
-  site:
-    process.env.VERCEL_ENV === 'production'
-      ? 'https://brutal.elian.codes/'
-      : process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}/`
-      : 'https://localhost:3001/',
+  site: 'https://inu-hacker.github.io',
+  base: '/inu-web',
   trailingSlash: 'ignore',
   integrations: [sitemap(), UnoCSS({ injectReset: true })],
 });
